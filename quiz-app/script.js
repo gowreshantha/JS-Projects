d5ab07a377182258edae1e82739088b470779fba
Gowresh N <gowreshantha@gmail.com>
2022-02-01 02:42:47 +0530

    Quiz-App

---
+++ quiz-app/script.js
@@ -0,0 +1,44 @@
+const quizData = [
+    {
+        question: "Which type of JavaScript language is ___",
+        a:"Object-Oriented",
+        b:"Object-Based",
+        c:"Assembly-language",
+        d:"High-level",
+        correct:"a"
+    },
+    {
+        question: "Which one of the following also known as Conditional Expression:",
+        a:"Alternative to if-else",
+        b:"Switch statement",
+        c:"If-then-else statement",
+        d:"immediate if",
+        correct:"d"
+    },
+    {
+        question: "In JavaScript, what is a block of statement?",
+        a:"Conditional block",
+        b:"block that combines a number of statements into a single compound statement",
+        c:"both conditional block and a single statement",
+        d:"block that contains a single statement",
+        correct:"b"
+    },
+    {
+        question: "The \"function\" and \" var\" are known as:",
+        a:"Keywords",
+        b:"Data types",
+        c:"Declaration statements",
+        d:"Prototypes",
+        correct:"c"
+    },
+    {
+        question: "Which of the following variables takes precedence over the others if the names are the same?",
+        a:"Global variable",
+        b:"The local element",
+        c:"The two of the above",
+        d:"None of the above",
+        correct:"b"
+    }
+];
+
+let currentQuestion = 0;
