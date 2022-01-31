adedfc5d63639ffd5a08f0da30a5a2b9ca0316de
Gowresh N <gowreshantha@gmail.com>
2022-02-01 00:35:50 +0530

    CountDown Timer Project

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,22 +1,36 @@
-console.log("new Date() = "+new Date());
-
-//Creatng a specific date object in JavaScript using a date string
-var dateOfBirth = new Date("September 13, 1991 04:15:00");
-console.log(dateOfBirth);
-
-
 
 function countdown(){
-    var currentYear = new Date();
-    console.log(currentYear.getFullYear());
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    var nextNewYear = currentYear + 1;
+
+    var newYear = new Date("01/01/"+nextNewYear);
+
+    let diffTime = Math.abs(newYear - currentDate);
+
+    //Days
+    let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    document.getElementById("days").innerHTML = diffDays;
+
+    //Seconds -- Convert ms to seconds
+    let seconds = diffTime / 1000;
+
+    //Hours
+    let hours = parseInt(seconds / 3600); //3600 seconds in 1 hour
+    seconds = seconds % 3600; // seconds remaining after extracting hours
+    document.getElementById("hours").innerHTML = hours;
+
+    //Minutes
+    let minutes = parseInt( seconds / 60 ); // 60 seconds in 1 minute
+    document.getElementById("mins").innerHTML = minutes;
+
+    //Keep only seconds not extracted to minutes:
+    seconds = parseInt(seconds % 60);
+    document.getElementById("seconds").innerHTML = seconds;
 }
 
-countdown();
+setInterval(countdown, 1000);
 
 
-const date1 = new Date('7/13/2010');
-const date2 = new Date('12/15/2010');
-const diffTime = Math.abs(date2 - date1);
-const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
-console.log(diffTime + " milliseconds");
-console.log(diffDays + " days");
+
+
