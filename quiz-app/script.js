f1aba462b91df3e42ecaabf00906d6126f511bad
Gowresh N <gowreshantha@gmail.com>
2022-02-02 02:04:07 +0530

    Quiz app completed

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -42,3 +42,65 @@ const quizData = [
 ];
 
 let currentQuestion = 0;
+
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const option_a = document.getElementById("option_a");
+const option_b = document.getElementById("option_b");
+const option_c = document.getElementById("option_c");
+const option_d = document.getElementById("option_d");
+const submitBtn = document.getElementById("submitBtn");
+const answers = document.querySelectorAll(".answer");
+
+let currentQuiz = 0;
+let score = 0;
+loadQuiz();
+function loadQuiz(){
+        deSelectAnswers();
+        question.innerText = quizData[currentQuiz].question;
+        option_a.innerText = quizData[currentQuiz].a;
+        option_b.innerText = quizData[currentQuiz].b;
+        option_c.innerText = quizData[currentQuiz].c;
+        option_d.innerText = quizData[currentQuiz].d;
+}
+
+submitBtn.addEventListener('click', function(){
+   //Check to see the answer
+    let answer = getSelected();
+
+    //If answer is selected continue or do nothing
+    if(answer)
+    {
+        //If answer is correct increment the score
+        if(answer === quizData[currentQuiz].correct)
+        {
+            score++;
+        }
+        currentQuiz++;
+        if(currentQuiz < quizData.length)
+        {
+            loadQuiz();
+        }else{
+        quiz.innerHTML = `<h2>You answered correctly ${score}/${quizData.length} questions. </h2>
+                            <button onclick='location.reload()'>Reload</button>`;
+        } 
+    }
+});
+
+//To get selected answer
+function getSelected(){
+    let answerSelected = undefined;
+    answers.forEach((answer)=>{
+        if(answer.checked){
+            answerSelected = answer.id;
+        }
+    });
+    return answerSelected;
+}
+
+//To de-select answers
+function deSelectAnswers(){
+    answers.forEach((answer)=>{
+        answer.checked = false;
+    });
+}
